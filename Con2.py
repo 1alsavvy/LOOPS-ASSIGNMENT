81b4102dd85ea268ab5ec85597cd8a819074ba31
1alsavvy <ekpefrederick69@gmail.com>
2022-09-09 16:37:44 +0000

    solutions to loops assignment

---
+++ Con2.py
@@ -0,0 +1,5 @@
+sum = 0
+for x in range(11):
+    sum +=  x
+    print(sum)
+    
---
+++ Con4.py
@@ -0,0 +1,5 @@
+fact = 1
+num = int(input("Enter any positive integer: "))
+for x in range(num):    
+    fact = 
+    print(fact)
